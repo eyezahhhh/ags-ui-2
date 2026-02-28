f575504360d947e0c282881e4d79c987a4cb2eb7
eyezah (Gigabox) <git@eyezah.com>
2026-02-28 15:31:20 +1300

    fix example wallpaper directory path

--- src/constants/wallpaper-directory.const.ts
+++ src/constants/wallpaper-directory.const.ts
@@ -1,8 +1,9 @@
 // @ts-nocheck
 
+import { ROOT } from "./root.const";
 import { WORKING_DIRECTORY } from "./working-directory.const";
 
 export const WALLPAPER_DIRECTORY =
 	typeof WALLPAPER_DIR == "string"
 		? WALLPAPER_DIR
-		: `${WORKING_DIRECTORY}/example-wallpapers`;
+		: `${ROOT}/example-wallpapers`;
